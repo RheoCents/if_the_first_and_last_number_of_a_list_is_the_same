6d250facc0aa9941376acf1696a28b8b60763ac0
unknown <centinaraphael0157@gmail.com>
2024-01-25 00:44:12 +0800

    trying code upload

---
+++ if_the_first_and_last_number_of_a_list_is_the_same.py
@@ -0,0 +1,6 @@
+#Write a function to return True if the first and last number of a given list is same. 
+#If numbers are different then return False.
+
+given_number_1 = [10, 20, 30, 40, 10]
+given_number_2 = [75, 65, 35, 75, 30]
+ 
