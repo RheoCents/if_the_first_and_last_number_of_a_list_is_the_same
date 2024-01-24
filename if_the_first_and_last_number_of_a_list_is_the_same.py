f899e2cd970737c58680882d7f345f0f5c4ba80b
unknown <centinaraphael0157@gmail.com>
2024-01-25 01:13:28 +0800

    Code Worked (but it is not that layered or complicated)

--- if_the_first_and_last_number_of_a_list_is_the_same.py
+++ if_the_first_and_last_number_of_a_list_is_the_same.py
@@ -2,7 +2,21 @@
 #If numbers are different then return False.
 given_list_1 = [10, 20, 30, 40, 10]
 given_list_1_result = str()
+given_list_1_len = len(given_list_1)
+
 given_list_2 = [75, 65, 35, 75, 30]
 given_list_2_result = str()
-if given_list_1[0] / given_list_1[4] == 1:
-    print('code working')
+given_list_2_len = len(given_list_2)
+
+if given_list_1[0] / given_list_1[given_list_1_len-1] == 1:
+    given_list_1_result = 'True'
+else:
+    given_list_1_result = 'False'
+
+if given_list_2[0] / given_list_2[given_list_2_len-1] == 1:
+    given_list_2_result = 'True'
+else:
+    given_list_2_result = 'False'
+
+print(given_list_1_result)
+print(given_list_2_result)
