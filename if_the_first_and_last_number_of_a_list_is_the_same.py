86fd1aab0c198d0ac7843061679e4bd6d0584f18
unknown <centinaraphael0157@gmail.com>
2024-01-25 01:28:24 +0800

    trying def and learning it

--- if_the_first_and_last_number_of_a_list_is_the_same.py
+++ if_the_first_and_last_number_of_a_list_is_the_same.py
@@ -1,22 +1,18 @@
 #Write a function to return True if the first and last number of a given list is same. 
 #If numbers are different then return False.
 given_list_1 = [10, 20, 30, 40, 10]
-given_list_1_result = str()
 given_list_1_len = len(given_list_1)
 
 given_list_2 = [75, 65, 35, 75, 30]
-given_list_2_result = str()
 given_list_2_len = len(given_list_2)
 
-if given_list_1[0] / given_list_1[given_list_1_len-1] == 1:
-    given_list_1_result = 'True'
-else:
-    given_list_1_result = 'False'
 
-if given_list_2[0] / given_list_2[given_list_2_len-1] == 1:
-    given_list_2_result = 'True'
-else:
-    given_list_2_result = 'False'
+def given_list_result(given_list):
+    if given_list[0] / given_list[given_list_1_len-1] == 1:
+        return True
+    else:
+        return False
+given_list_1 = [10, 20, 30, 40, 10]
 
-print(given_list_1_result)
-print(given_list_2_result)
+
+print('The first given list is', given_list_result(given_list_1))
