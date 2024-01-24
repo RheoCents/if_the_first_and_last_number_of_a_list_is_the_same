c4ce571d7beebdc02ced4284010551564a4dc874
unknown <centinaraphael0157@gmail.com>
2024-01-25 01:08:06 +0800

    trying some base codes

--- if_the_first_and_last_number_of_a_list_is_the_same.py
+++ if_the_first_and_last_number_of_a_list_is_the_same.py
@@ -1,6 +1,8 @@
 #Write a function to return True if the first and last number of a given list is same. 
 #If numbers are different then return False.
-
-given_number_1 = [10, 20, 30, 40, 10]
-given_number_2 = [75, 65, 35, 75, 30]
- 
+given_list_1 = [10, 20, 30, 40, 10]
+given_list_1_result = str()
+given_list_2 = [75, 65, 35, 75, 30]
+given_list_2_result = str()
+if given_list_1[0] / given_list_1[4] == 1:
+    print('code working')
